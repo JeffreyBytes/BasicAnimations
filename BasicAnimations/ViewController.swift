4b7d9637960c1fcb7ee6e21c7fb94c203cc12b1d
Jeffrey Carpenter <spaz926@me.com>
2019-05-15 11:32:11 -0500

    Add subview with constraints to main VC

--- BasicAnimations/ViewController.swift
+++ BasicAnimations/ViewController.swift
@@ -12,9 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(redView)
+        
+        // Add constraints
+        
+        let redLeadingContraint = NSLayoutConstraint(item: redView,
+                                                     attribute: .leading,
+                                                     relatedBy: .equal,
+                                                     toItem: view.safeAreaLayoutGuide,
+                                                     attribute: .leading,
+                                                     multiplier: 1,
+                                                     constant: 30)
+        let redBottomContraint = NSLayoutConstraint(item: redView,
+                                                    attribute: .bottom,
+                                                    relatedBy: .equal,
+                                                    toItem: view.safeAreaLayoutGuide,
+                                                    attribute: .bottom,
+                                                    multiplier: 1,
+                                                    constant: 0)
+        let redHeightContraint = NSLayoutConstraint(item: redView,
+                                                    attribute: .height,
+                                                    relatedBy: .equal,
+                                                    toItem: nil,
+                                                    attribute: .notAnAttribute,
+                                                    multiplier: 1,
+                                                    constant: 100)
+        let redViewAspectRatioConstraint = NSLayoutConstraint(item: redView,
+                                                              attribute: .width,
+                                                              relatedBy: .equal,
+                                                              toItem: redView,
+                                                              attribute: .height,
+                                                              multiplier: 1,
+                                                              constant: 0)
+        NSLayoutConstraint.activate([redLeadingContraint, redBottomContraint, redHeightContraint, redViewAspectRatioConstraint])
     }
-
-
 }
 
